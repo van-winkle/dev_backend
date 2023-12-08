25e7284c1a77c94562bfeeb2be183ba37c86d4ee
AlejandrArgueta <santos.argueta@dgehm.gob.sv>
2023-12-08 21:23:49 +0100

    carpeta Phones creadad

---
+++ app/Models/Phones/PhoneModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Phones;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PhoneModel extends Model
+{
+    use HasFactory;
+}

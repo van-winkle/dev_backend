878205b23acf0a529b55988316dd588952587ba8
AlejandrArgueta <santos.argueta@dgehm.gob.sv>
2023-12-11 21:28:21 +0100

    cambios

--- app/Models/Phones/PhoneModel.php
+++ app/Models/Phones/PhoneModel.php
@@ -4,10 +4,11 @@ namespace App\Models\Phones;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PhoneModel extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'pho_phone_models';
     protected $fillable = ['name','active','pho_phone_brand_id','deleted_at'];
 }
--- database/seeders/PhonesSeeder.php
+++ database/seeders/PhonesSeeder.php
@@ -14,11 +14,11 @@ class PhonesSeeder extends Seeder
     public function run(): void
     {
         DB::table('pho_phones')->insert(
-            [['number' => 101, 'type'=> 'movil','imei' => 101, 'price' => 100,'active' => true, 'adm_employee_id' => 1,'pho_phone_plan_id'=>1,'pho_phone_contract_id'=>1,'pho_phone_model_id'=>1],
-            ['number' => 102, 'type'=> 'movil','imei' => 102, 'price' => 200,'active' => true, 'adm_employee_id' => 2,'pho_phone_plan_id'=>2,'pho_phone_contract_id'=>2,'pho_phone_model_id'=>2],
-            ['number' => 103, 'type'=> 'movil','imei' => 104, 'price' => 300,'active' => true, 'adm_employee_id' => 3,'pho_phone_plan_id'=>3,'pho_phone_contract_id'=>3,'pho_phone_model_id'=>3],
-            ['number' => 104, 'type'=> 'movil','imei' => 105, 'price' => 400,'active' => true, 'adm_employee_id' => 4,'pho_phone_plan_id'=>4,'pho_phone_contract_id'=>4,'pho_phone_model_id'=>4],
-            ['number' => 105, 'type'=> 'movil','imei' => 106, 'price' => 500,'active' => true, 'adm_employee_id' => 5,'pho_phone_plan_id'=>5,'pho_phone_contract_id'=>5,'pho_phone_model_id'=>5]]
+            [['number' => '101', 'type'=> 'movil','imei' => 101, 'price' => 100,'active' => true, 'adm_employee_id' => 1,'pho_phone_plan_id'=>1,'pho_phone_contract_id'=>1,'pho_phone_model_id'=>1],
+            ['number' => '102', 'type'=> 'movil','imei' => 102, 'price' => 200,'active' => true, 'adm_employee_id' => 2,'pho_phone_plan_id'=>2,'pho_phone_contract_id'=>2,'pho_phone_model_id'=>2],
+            ['number' => '103', 'type'=> 'movil','imei' => 104, 'price' => 300,'active' => true, 'adm_employee_id' => 3,'pho_phone_plan_id'=>3,'pho_phone_contract_id'=>3,'pho_phone_model_id'=>3],
+            ['number' => '104', 'type'=> 'movil','imei' => 105, 'price' => 400,'active' => true, 'adm_employee_id' => 4,'pho_phone_plan_id'=>4,'pho_phone_contract_id'=>4,'pho_phone_model_id'=>4],
+            ['number' => '105', 'type'=> 'movil','imei' => 106, 'price' => 500,'active' => true, 'adm_employee_id' => 5,'pho_phone_plan_id'=>5,'pho_phone_contract_id'=>5,'pho_phone_model_id'=>5]]
         );
     }
 }

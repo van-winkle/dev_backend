cd6ef6cc8e4301ebacba8c153b7d7dec6faed748
AlejandrArgueta <santos.argueta@dgehm.gob.sv>
2023-12-11 22:58:00 +0100

    fix errors, contact and contract

--- app/Models/Phones/PhoneContact.php
+++ app/Models/Phones/PhoneContact.php
@@ -4,14 +4,19 @@ namespace App\Models\Phones;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PhoneContact extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'dir_contacts';
-    protected $fillable = ['name','deleted_at'];
+    protected $primarykey = 'id';
+    protected $keyType = 'int';
+    public $incrementing = true;
+    
+    protected $fillable = ['name'];
 
     public function contracts (){
-        return $this->hasMany(PhoneContract::class, 'id');
+        return $this->hasMany(PhoneContract::class, 'dir_contact_id');
     }
 }

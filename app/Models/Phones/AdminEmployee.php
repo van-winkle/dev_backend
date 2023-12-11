3d9a2d01b88d38f3f2caa2a28781cfcd0bef9649
Cristopher-Developer <Cristopher.molina@dgehm.gob.sv>
2023-12-11 10:15:36 -0600

    agregue modelos

---
+++ app/Models/Phones/AdminEmployee.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Phones;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminEmployee extends Model
+{
+    use HasFactory;
+    protected $table = 'adm_employees';
+    protected $fillable = ['name','deleted_at'];
+}

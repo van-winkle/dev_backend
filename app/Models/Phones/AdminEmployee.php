baed8b2649b7c5dc1d5a337e362422e7457d8cde
van-winkle <152640259+van-winkle@users.noreply.github.com>
2023-12-12 10:15:48 -0600

    FilesUpdate

--- app/Models/Phones/AdminEmployee.php
+++ app/Models/Phones/AdminEmployee.php
@@ -8,8 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class AdminEmployee extends Model
 {
     use HasFactory;
-    protected $table = 'adm_employees';
     
+    protected $table = 'adm_employees';
+
     protected $fillable = [
         'name',
         'deleted_at'

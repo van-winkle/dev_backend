a759f0109a58ae3d7bcb532553ee72f3e19e9128
Cristopher-Developer <Cristopher.molina@dgehm.gob.sv>
2023-12-12 08:48:40 -0600

    modificacion de modelo Brand

--- app/Models/Phones/PhoneBrand.php
+++ app/Models/Phones/PhoneBrand.php
@@ -4,14 +4,45 @@ namespace App\Models\Phones;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PhoneBrand extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     // name the table
     protected $table = 'pho_phone_brands';
+
+    protected $primaryKey= "id";
+
+    protected $KeyType = "int";
+
+    public $incrementing = true;
     // Table Fields
     protected $fillable = [
-        'name', 'active', 'deleted_at',
+        'name',
+        'active',
     ];
+
+    public $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $casts = [];
+
+
+
+    protected static $recordEvents = [
+        'created',
+        'updated',
+        'deleted',
+    ];
+
+    public function Brand ()
+    {
+        return $this->hasMany(PhoneBrand::class,'pho_phone_model_id');
+    }
+
+
 }

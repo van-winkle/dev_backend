017f4488e38da9d5400d0f08a43fcffcbf97abee
AlejandrArgueta <santos.argueta@dgehm.gob.sv>
2023-12-15 11:38:16 -0600

    Table Incidents Categories

---
+++ app/Models/Phones/IncidentsCategory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models\Phones;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class IncidentsCategory extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $table = 'pho_phone_incident_categories';
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'int';
+
+    public $incrementing = true;
+
+    public $fillable = [
+        'name',
+        'active',
+    ];
+
+    public $hidden = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    protected $casts = [];
+
+    protected static $recordEvents = [
+        'created',
+        'updated',
+        'deleted',
+    ];
+
+ }

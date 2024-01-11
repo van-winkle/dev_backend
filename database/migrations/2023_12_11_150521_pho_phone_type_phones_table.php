d92df0ec7f6c7678fa366646a33b1e367cb6e331
van-winkle <152640259+van-winkle@users.noreply.github.com>
2024-01-11 13:08:04 -0600

    NewFiles-TypePhones&UpdatePlansController

---
+++ database/migrations/2023_12_11_150521_pho_phone_type_phones_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pho_phone_type_phones', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('active')->default(true);
+
+            $table->timestamp('created_at', 0)->useCurrent();
+            $table->timestamp('updated_at', 0)->useCurrent();
+            $table->unique(['name', 'deleted_at']);
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pho_phone_type_phones');
+    }
+};
